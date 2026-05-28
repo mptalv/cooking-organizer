570a59a628f2b8eecd3339730dc678c9273d510e
mptalv <mptalv89@gmail.com>
2026-05-28 10:39:35 -0400

    updated main

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# FastAPI open here
-
 from fastapi import FastAPI
+
+from database.database import Base
+from database.database import engine
 from routes.orders import router as orders_router
-from data.recipes import recipes
+from routes.recipes import router as recipes_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(recipes_router)
+app.include_router(orders_router)
 
 
 @app.get("/")
 def home():
-    return {"message": "Kitchen API running"}
-
-@app.get("/recipes")
-def get_recipes():
-    return recipes
+    return {
+        "message": "Kitchen Manager API Running"
+    }
